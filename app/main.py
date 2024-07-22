c00caaf1184d19739d18744dbb122c2e6842dff8
Aleksandr Mazur <a9p9m9@gmail.com>
2024-07-22 13:35:14 +0300

    solution_fix_2

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: int | float, y: int | float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: int | float, y_coord: int | float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: int | float) -> Vector:
         return Vector((self.x + other.x), (self.y + other.y))
@@ -33,7 +33,9 @@ class Vector:
         return math.sqrt(pow(self.x, 2) + pow(self.y, 2))
 
     def get_normalized(self) -> Vector:
-        return Vector(self.x / self.get_length(), self.y / self.get_length())
+        return Vector(
+            self.x / self.get_length(), self.y / self.get_length()
+        )
 
     def angle_between(self, other: int | float) -> int | float:
         return round(
